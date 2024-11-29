c4dd4bcf1fc172d6c88cfdd91773c0abe7cd60c9
Boris <bptobar@sudamericano.edu.ec>
2024-11-29 09:03:12 -0500

    Primer commit

---
+++ proceso_seleccion/src/main/kotlin/com/example/proceso_seleccion/ProcesoSeleccionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.proceso_seleccion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProcesoSeleccionApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProcesoSeleccionApplication>(*args)
+}
---
+++ proceso_seleccion/src/test/kotlin/com/example/proceso_seleccion/ProcesoSeleccionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.proceso_seleccion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProcesoSeleccionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
